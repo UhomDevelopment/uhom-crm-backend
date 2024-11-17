d62e94fd673d88c620c4c611c1b067ff6393972f
dreamonovich <dreamonovich@gmail.com>
2024-11-18 00:38:07 +0300

    fix imports

--- crm/video/urls.py
+++ crm/video/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from video.views import AddCameraView, RetrieveUpdateDestroyCameraView, ListCameraView, RetrieveUpdateDestroyVideoView, \
-    ListVideoView, ListCameraTimecodesView, VideoUploadView
+    ListVideoView, ListCameraTimecodesView, AddVideoView
 
 urlpatterns = [
     path('camera/add/', AddCameraView.as_view(), name='add'),
     path('camera/<int:pk>', RetrieveUpdateDestroyCameraView.as_view()),
     path('cameras', ListCameraView.as_view()),
     path('video/<int:pk>', RetrieveUpdateDestroyVideoView.as_view()),
-    path('video/upload', VideoUploadView.as_view()),
+    path('video/upload', AddVideoView.as_view()),
     path('videos/<int:cam_pk>', ListVideoView.as_view()),
     path('videos/', ListVideoView.as_view()),
     path('timecodes/<int:pk>', ListCameraTimecodesView.as_view()),
